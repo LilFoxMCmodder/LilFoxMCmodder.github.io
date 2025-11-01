c6bc473e1cd2d19a9269df5cd6a784cfd9d86071
LilFoxMCmodder <onceuponabrokenheartfan@gmail.com>
2025-10-31 23:21:51 -0400

    Add Home1.html and Javascript1.js for login functionality; update index.html comment formatting

---
+++ Javascript1.js
@@ -0,0 +1,11 @@
+function checklogin () {
+    //this would get the users input
+    let username = document.getElementById("username").value; 
+    let password = document.getElementById("password").value;
+
+    //this checks if the username and password are correct
+    if (username === "admin" && password === "password") {
+        // if correct, redirect to Home1.html
+        window.location.href = "Home1.html";
+    }
+}
